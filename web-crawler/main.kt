cbd04a2e268bd747eb21382137a0d323c1688f05
yeshmukhanbet <daulet.yeshmukhanbet@deliveryhero.com>
2022-05-16 02:09:54 +0200

    Refactoring of Web Crawler. This commit refactors the actual implementation of Web Crawler to accept list of urls as an input and process each of them in parallel. Also, it adds instructions how to run the crawler.

--- web-crawler/main.kt
+++ web-crawler/main.kt
@@ -1,50 +1,72 @@
+import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.Executors
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 fun main() {
-    BasicWebCrawler().crawl(url = "https://google.com")
+    BasicWebCrawler().crawl(
+        urls = listOf(
+            "https://google.com",
+            "https://facebook.com",
+            "https://linkedin.com"
+        ),
+    )
 }
 
 data class Node(val url: String, val depth: Int)
 
 class BasicWebCrawler {
-    private val queue = ArrayDeque<Node>()
-    private val visited = HashSet<String>()
+
+    fun crawl(urls: List<String>) {
+        val threadPool = Executors.newFixedThreadPool(8)
+        urls.forEach {
+            threadPool.execute(WebCrawlerWorker(baseUrl = it))
+        }
+        threadPool.shutdown()
+    }
+}
+
+class WebCrawlerWorker(private val baseUrl: String) : Runnable {
     private val regexp = "(http|https)://(\\w+\\.)+(com|org)"
     private val pattern: Pattern = Pattern.compile(regexp)
-    private val maxDepth = 2
+    private val maxPages = 100 // max number of unique pages to be visited
 
-    fun crawl(url: String) {
+    override fun run() {
+        crawl(url = baseUrl)
+    }
+
+    private fun crawl(url: String) {
+        val queue = ArrayDeque<Node>()
+        val visited = HashSet<String>()
         queue.addLast(Node(url = url, depth = 0))
         visited.add(url)
-        var cnt = 0
-        while(!queue.isEmpty()) {
+        var cnt = 1
+        while (!queue.isEmpty()) {
             val (link, depth) = queue.removeFirst()
-            if(depth < maxDepth) {
-                val stream = runCatching {
-                    val urlConnection = URL(link).openConnection()
-                    urlConnection.connectTimeout = 500
-                    urlConnection.readTimeout = 1000
-                    urlConnection.getInputStream()
-                }
+            println("link->$link")
+            val stream = runCatching {
+                val urlConnection = URL(link).openConnection() as HttpURLConnection
+                urlConnection.connectTimeout = 500
+                urlConnection.readTimeout = 1000
+                urlConnection.inputStream
+            }
+            stream.onSuccess { inputStream ->
+                inputStream.use {
+                    val rawHtml = it.bufferedReader().readText()
 
-                stream.onSuccess { inputStream ->
-                    inputStream.use {
-                        val rawHtml = it.bufferedReader().readText()
-
-                        val matcher: Matcher = pattern.matcher(rawHtml)
-                        while (matcher.find()) {
-                            val href: String = matcher.group()
-                            if (!visited.contains(href)) {
-                                visited.add(link)
-                                queue.addLast(Node(url = href, depth = depth + 1))
-                            }
+                    val matcher: Matcher = pattern.matcher(rawHtml)
+                    while (matcher.find()) {
+                        val href: String = matcher.group()
+                        if (!visited.contains(href) && cnt < maxPages) {
+                            visited.add(href)
+                            queue.addLast(Node(url = href, depth = depth + 1))
+                            cnt++
                         }
                     }
-                }.onFailure {
-                    println("Failed to scrape the WEB resource[url='$url', error='$it']")
                 }
+            }.onFailure {
+                println("Failed to scrape the WEB resource[url='$url', error='$it']")
             }
         }
     }
