89c9028b433b676c4852327968652cddb8be4e72
yeshmukhanbet <daulet.yeshmukhanbet@deliveryhero.com>
2022-06-02 00:58:06 +0200

    Implementation of the Bloom Filter. This commit implements the Bloom Filter data structure.

---
+++ bloom-filter/Main.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    List<HashFunction> hashFunctions = new ArrayList<>();
+	    hashFunctions.add(new HashFunction<Integer>() {
+	        @Override
+	        public int hash(Integer obj) {
+	            int hash = 7;
+                hash = 31 * hash + (obj == null ? 0 : obj.hashCode());
+                return Math.abs(hash);
+	        }
+	    });
+	    hashFunctions.add(new HashFunction<Integer>() {
+	        @Override
+	        public int hash(Integer obj) {
+	            long hash = obj.hashCode() + 911382323L;
+                return Math.abs((int)(hash % 972663749L));
+	        }
+	    });
+	    hashFunctions.add(new HashFunction<Integer>() {
+	        @Override
+	        public int hash(Integer obj) {
+	            int hash = obj.hashCode();
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+                hash += hash << 13;
+                hash ^= hash >> 11;
+                hash += hash << 15;
+                return Math.abs(hash);
+	        }
+	    });
+	    
+	    int capacity = 1000;
+		BloomFilter<Integer> filter = new BloomFilter(1000, hashFunctions);
+		for (int i=0; i<400; i++) {
+		    filter.add(i);
+		}
+		
+		System.out.println(filter.mightContain(2)); // true positive
+		System.out.println(filter.mightContain(89)); // true positive
+		System.out.println(!filter.mightContain(971)); // true negative
+		System.out.println(!filter.mightContain(888)); // true negative
+	}
+	
+	private static class BloomFilter<T> {
+	    private int capacity;
+	    private int[] set;
+	    private List<HashFunction> hashFunctions;
+	    
+	    BloomFilter(int capacity, List<HashFunction> hashFunctions) {
+	        this.capacity = capacity;
+	        this.set = new int[capacity];
+	        if(hashFunctions == null || hashFunctions.isEmpty()) { 
+	            throw new RuntimeException("At least one hash function is required.");
+	        }
+	        this.hashFunctions = hashFunctions;
+	    }
+	    
+	    // inserts the value into the set
+	    public void add(T value) {
+	        for (HashFunction f : hashFunctions) {
+	            this.set[f.hash(value) % this.capacity] = 1;
+	        }
+	    }
+	    
+	    // Returns false, when the value is definitely not in the set. This is a true negative.
+        // and true, if it might be in the set. This could be a false positive, or it could be a true positive.
+	    public boolean mightContain(T value) {
+	        for (HashFunction f : hashFunctions) {
+	             if(this.set[f.hash(value) % this.capacity] == 0) {
+	                 return false;
+	             }
+	        }
+	        
+	        return true;
+	    }
+	}
+	
+	interface HashFunction<T> {
+	    int hash(T obj);
+	}
+}
+
